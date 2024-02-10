2a8f8aa6dfccde456b529a3541a7a0200892c87a
Tom C <crvntst@gmail.com>
2024-02-10 17:20:48 -0500

    tweaked gifs and made it in js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const phrases = [
+  "no",
+  "are you sure??",
+  "please",
+  "pleaseeee?",
+  "be fr baby",
+  "stop hitting no",
+  "reconsider",
+  "r u serious",
+  "tiny will be mad",
+];
 
 function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  function handleNoClick() {
+    setNoCount(noCount + 1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="valentine-container">
+      {yesPressed ? (
+        <>
+          <img
+            alt="kittycat"
+            src="https://media.tenor.com/DpJdyKQKgYkAAAAi/cat-jump.gif"
+          />
+          <div className="text">RAAAAAAHHHHHH</div>
+        </>
+      ) : (
+        <>
+          <img
+            alt="kittycat2"
+            src="httpshttps://storage.ko-fi.com/cdn/useruploads/display/051b1076-5d8a-44e9-a7b2-ed2bd9b91b4f_popcat_white.gif://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHZ4bWh6azMwcHRtdGwycGMyeDB0bGRhdHhscjhoOGl5MW03Z2hxZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QAn6JoVuABBEapYyX2/giphy.gif"
+          />
+          <div>will you be my valentine?</div>
+          <div>
+            <button
+              className="yesButton"
+              style={{ fontSize: `${yesButtonSize}px` }}
+              onClick={() => setYesPressed(true)}
+            >
+              YES
+            </button>
+            <button onClick={handleNoClick} className="noButton">
+              {getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
